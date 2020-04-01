51e3f90dd204f6349e2ade9ac948e92e86d41f0f
Ricardo Redivo <redivo@gmail.com>
2020-04-02 00:17:41 +0200

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,21 @@
+{
+  "name": "CellEdit",
+  "version": "1.0.20",
+  "homepage": "https://github.com/cosmopolit/CellEdit",
+  "authors": [
+    "Rico Redivo <redivo@general-informatics.com>"
+  ],
+  "description": "inline edit cell content for datatables",
+  "main": "js/dataTables.cellEdit.js",
+  "keywords": [
+    "datatables",
+    "celledit"
+  ],
+  "license": "MIT",
+  "ignore": [
+    "**/.*",
+    "example"
+  ],
+  "devDependencies": {
+  }
+}
